08618b7eaa4992685044d7fa3ae5a65745ab023e
Martynka123-code <martyna.peukert@gmail.com>
2026-05-09 18:16:12 +0200

    rf_online.py file improvement.

--- models/rf_online.py
+++ models/rf_online.py
@@ -2,4 +2,4 @@ class RandomForestOnline:
     def __init__(self, model_path):
         pass
     def classify_stream(self, feature_vector):
-        print("Klasyfikacja online na podstawie modelu RF.")
+        print("Online classification based on the Random Forest model.")
